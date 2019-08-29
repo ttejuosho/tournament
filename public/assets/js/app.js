a4b92e8e8e5b25adc881446482b07d92fc17a0b8
Tejuosho <ttejuosho@sentinel.com>
2019-08-29 15:42:00 -0500

    total amount

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,57 +1,33 @@
-$(document).ready(function(){
-var tournamentTable;
-    function renderMemberTable(data){
+$(document).ready(function() {
+    var tournamentTable;
 
+    function renderMemberTable(data) {
         tournamentTable = $('#tournamentTable').DataTable({
             dom: "<'row justify-content-between'<'col-sm-12 col-md-2'lB><'col-sm-12 col-md-4'f>>" +
-                 "<'row'<'col-sm-12'tr>>" +
-                 "<'row'<'col-sm-12 col-md-4'i><'col-sm-12 col-md-8'p>>",
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-12 col-md-4'i><'col-sm-12 col-md-8'p>>",
             data: data,
             rowGroup: {
-                startRender: function(rows, group){
+                startRender: function(rows, group) {
                     return $('<tr/>').append('<td colspan="10" style="background-color: slategray; color: white;">' + group + '</td>');
                 },
                 dataSrc: 'TeamName'
             },
-            buttons: [
-                'copy', 'csv', 'excel', 'pdf', 'print'
-            ],
-            // drawCallback: function () {
-            //     var api = this.api();
-            //     $( api.table().footer() ).html(
-            //       api.column( 5, {page:'current'} ).data().sum()
-            //     );
-            //   },
-              footerCallback: function(row, data, start, end, display) {
-                var api = this.api();
-               
-                api.columns('.sum', {
-                  page: 'current'
-                }).every(function() {
-                  var sum = this
-                    .data()
-                    .reduce(function(a, b) {
-                      var x = parseFloat(a) || 0;
-                      var y = parseFloat(b) || 0;
-                      return x + y;
-                    }, 0);
-                  console.log(sum); //alert(sum);
-                  $(this.footer()).html(sum);
-                });
-              },
+            buttons: ['excel', 'pdf', 'print'],
             rowId: 'id',
             responsive: true,
-            order: [[2, 'asc']],
-            columns:[
-                {
+            order: [
+                [2, 'asc']
+            ],
+            columns: [{
                     data: null,
                     className: 'id'
                 },
                 {
                     data: 'Name',
                     className: 'Name text-center',
-                    render: function(data, type, row, meta){
-                        return '<a href="/member/'+ row.id +'">'+ data +'</a>';
+                    render: function(data, type, row, meta) {
+                        return '<a href="/member/' + row.id + '">' + data + '</a>';
                     }
                 },
                 {
@@ -68,40 +44,40 @@ var tournamentTable;
                 },
                 {
                     data: 'Amount',
-                    className: 'Amount text-center'
+                    className: 'Amount text-center',
+                    render: function(data, row, type, meta) {
+                        return parseInt(data);
+                    }
                 },
                 {
                     data: 'id',
                     className: 'text-center',
-                    render: function(data,row,type,meta){
+                    render: function(data, row, type, meta) {
                         return '<a href="/member/' + data + '/update" class="btn btn-warning btn-sm mr-2">Edit</a><a href="/member/' + data + '/delete" class="btn btn-danger btn-sm" onclick="return confirm(`Are you sure you want to delete this entry?`)">Delete</a>';
                     }
                 }
             ],
-            columnDefs: [ {
+            columnDefs: [{
                 searchable: false,
                 orderable: false,
                 targets: 0
-            } ],
+            }],
         });
 
-        $.fn.dataTable.Api.register( 'sum()', function ( ) {
-            return this.flatten().reduce( function ( a, b ) {
-                if ( typeof a === 'string' ) {
-                    a = a.replace(/[^\d.-]/g, '') * 1;
-                }
-                if ( typeof b === 'string' ) {
-                    b = b.replace(/[^\d.-]/g, '') * 1;
-                }    
-                return a + b;
-            }, 0 );
-        });
-
-        tournamentTable.on('order.dt search.dt', function () {
-            tournamentTable.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
-                cell.innerHTML = i+1;
+        tournamentTable.on('order.dt search.dt', function() {
+            tournamentTable.column(0, { search: 'applied', order: 'applied' }).nodes().each(function(cell, i) {
+                cell.innerHTML = i + 1;
             });
         }).draw();
+
+        var column = tournamentTable.column(5);
+        $(column.footer()).html(
+            column.data().reduce(function(a, b) {
+                return a + b;
+            })
+        );
+        $('#TotalText').removeClass('text-center');
+        $('#TotalAmount').removeClass('text-center');
     }
 
     $.ajax({
@@ -110,7 +86,7 @@ var tournamentTable;
         error: function() {
             callback();
         },
-        success: function(res) {           
+        success: function(res) {
             renderMemberTable(res);
             $("#searchTerm").selectize({
                 maxItems: 1,
@@ -128,7 +104,7 @@ var tournamentTable;
                     if (!query.length) return callback();
                     callback(res);
                 },
-                onChange: function(value){
+                onChange: function(value) {
                     $("#searchButton").attr("href", "/member/" + $('#searchTerm')[0].selectize.options[value].id);
                 }
             });
