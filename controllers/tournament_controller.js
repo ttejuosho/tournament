2e3dbca067cdd9648cf61c3036b36a58c4b18fc2
Tejuosho <ttejuosho@sentinel.com>
2019-08-30 09:57:43 -0500

    form jQuery Validate

--- controllers/tournament_controller.js
+++ controllers/tournament_controller.js
@@ -6,38 +6,36 @@ function formatPhoneNumber(phoneNumberString) {
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
     var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
     if (match) {
-      return match[1] + '-' + match[2] + '-' + match[3]
+        return match[1] + '-' + match[2] + '-' + match[3]
     }
     return null
-  }
+}
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.redirect('/index');
 });
 
-router.get('/index', (req,res) => {
+router.get('/index', (req, res) => {
     return res.render("index");
 });
 
-router.get('/admin', (req,res) => {
+router.get('/admin', (req, res) => {
     db.TeamMember.findAll({})
-    .then( (dbTeamMember) => {
-        var hbsObject = {
-            teamMember: dbTeamMember
-        };
-        return res.render("admin", hbsObject);
-    });
+        .then((dbTeamMember) => {
+            var hbsObject = {
+                teamMember: dbTeamMember
+            };
+            return res.render("admin", hbsObject);
+        });
 });
 
-router.post('/member/new', (req,res) => {
+router.post('/member/new', (req, res) => {
     db.TeamMember.findOne({
         where: {
             Name: req.body.Name
         }
     }).then((dbTeamMember) => {
-        var formattedPhoneNumber = formatPhoneNumber(req.body.PhoneNumber);
-        console.log(formattedPhoneNumber);
-        if (dbTeamMember == null){
+        if (dbTeamMember == null) {
             db.TeamMember.create({
                 Name: req.body.Name,
                 TeamName: req.body.TeamName,
@@ -49,25 +47,25 @@ router.post('/member/new', (req,res) => {
             }).catch((err) => {
                 res.render('error', err);
             });
-    } else {
-        var err = {
-            error: dbTeamMember.Name.toUpperCase() + " already exists in the database"
+        } else {
+            var err = {
+                error: dbTeamMember.Name.toUpperCase() + " already exists in the database"
+            }
+            res.render("index", err);
         }
-        res.render("index",err);
-    }
     });
 });
 
-router.get('/member/:id/update', (req,res) => {
+router.get('/member/:id/update', (req, res) => {
     db.TeamMember.findByPk(req.params.id)
-         .then((dbTeamMember) => {
-        res.render('update', dbTeamMember.dataValues);
-    }).catch((err) => {
-        res.render('error', err);
-    });
+        .then((dbTeamMember) => {
+            res.render('update', dbTeamMember.dataValues);
+        }).catch((err) => {
+            res.render('error', err);
+        });
 });
 
-router.put('/member/:id/update', (req,res) => {
+router.put('/member/:id/update', (req, res) => {
     const dbTeamMember = {
         Name: req.body.Name,
         TeamName: req.body.TeamName,
@@ -76,37 +74,39 @@ router.put('/member/:id/update', (req,res) => {
         Amount: req.body.Amount
     };
 
-    db.TeamMember.update( dbTeamMember, { 
+    db.TeamMember.update(dbTeamMember, {
         where: {
             id: req.params.id
         }
-    }).then( (dbTeamMember) => {
+    }).then((dbTeamMember) => {
         res.redirect('/admin');
-    }).catch((err) => {
-		res.render('error', err);
-	});
-});
-
-router.get('/member/:id/delete', (req,res) => {
-    db.TeamMember.findByPk(req.params.id)
-            .then( (dbTeamMember) => {
-                db.TeamMember.destroy({
-        where: {
-            id: dbTeamMember.dataValues.id
-        }
-    }).then( () => { res.redirect('/admin');
-    }).catch((err) => { res.render('error', err)
-    })
-});
-});
-
-router.get('/member/:id', (req,res)=>{
-    db.TeamMember.findByPk(req.params.id)
-            .then((dbTeamMember) => {
-        res.render('view', dbTeamMember.dataValues);
     }).catch((err) => {
         res.render('error', err);
     });
+});
+
+router.get('/member/:id/delete', (req, res) => {
+    db.TeamMember.findByPk(req.params.id)
+        .then((dbTeamMember) => {
+            db.TeamMember.destroy({
+                where: {
+                    id: dbTeamMember.dataValues.id
+                }
+            }).then(() => {
+                res.redirect('/admin');
+            }).catch((err) => {
+                res.render('error', err)
+            })
+        });
+});
+
+router.get('/member/:id', (req, res) => {
+    db.TeamMember.findByPk(req.params.id)
+        .then((dbTeamMember) => {
+            res.render('view', dbTeamMember.dataValues);
+        }).catch((err) => {
+            res.render('error', err);
+        });
 });
 
 module.exports = router;
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,90 +1,91 @@
 $(document).ready(function() {
     var tournamentTable;
+
     function renderMemberTable(data) {
-        if ( !$.fn.DataTable.isDataTable( '#tournamentTable' ) ) {
-        tournamentTable = $('#tournamentTable').DataTable({
-            dom: "<'row justify-content-between'<'col-sm-12 col-md-2'lB><'col-sm-12 col-md-4'f>>" +
-                "<'row'<'col-sm-12'tr>>" +
-                "<'row'<'col-sm-12 col-md-4'i><'col-sm-12 col-md-8'p>>",
-            data: data,
-            rowGroup: {
-                startRender: function(rows, group) {
-                    return $('<tr/>').append('<td colspan="10" style="background-color: slategray; color: white;">Team Name: ' + group + '</td>');
+        if (!$.fn.DataTable.isDataTable('#tournamentTable')) {
+            tournamentTable = $('#tournamentTable').DataTable({
+                dom: "<'row justify-content-between'<'col-sm-12 col-md-2'lB><'col-sm-12 col-md-4'f>>" +
+                    "<'row'<'col-sm-12'tr>>" +
+                    "<'row'<'col-sm-12 col-md-4'i><'col-sm-12 col-md-8'p>>",
+                data: data,
+                rowGroup: {
+                    startRender: function(rows, group) {
+                        return $('<tr/>').append('<td colspan="10" style="background-color: slategray; color: white;">Team Name: ' + group + '</td>');
+                    },
+                    dataSrc: 'TeamName'
                 },
-                dataSrc: 'TeamName'
-            },
-            buttons: ['excel', 'pdf', 'print'],
-            rowId: 'id',
-            responsive: true,
-            order: [
-                [2, 'asc']
-            ],
-            columns: [{
-                    data: null,
-                    className: 'id'
-                },
-                {
-                    data: 'Name',
-                    className: 'Name text-center',
-                    render: function(data, type, row, meta) {
-                        return '<a href="/member/' + row.id + '">' + data + '</a>';
+                buttons: ['excel', 'pdf', 'print'],
+                rowId: 'id',
+                responsive: true,
+                order: [
+                    [2, 'asc']
+                ],
+                columns: [{
+                        data: null,
+                        className: 'id'
+                    },
+                    {
+                        data: 'Name',
+                        className: 'Name text-center',
+                        render: function(data, type, row, meta) {
+                            return '<a href="/member/' + row.id + '">' + data + '</a>';
+                        }
+                    },
+                    {
+                        data: 'TeamName',
+                        className: 'TeamName text-center',
+                        visible: false,
+                        searchable: true
+                    },
+                    {
+                        data: 'EmailAddress',
+                        className: 'EmailAddress text-center',
+                        render: function(data, type, row, meta) {
+                            return '<a href="mailto:' + data + '">' + data + '</a>';
+                        }
+                    },
+                    {
+                        data: 'PhoneNumber',
+                        className: 'PhoneNumber text-center'
+                    },
+                    {
+                        data: 'Amount',
+                        className: 'Amount text-center',
+                        render: function(data, row, type, meta) {
+                            return '$' + data;
+                        }
+                    },
+                    {
+                        data: 'id',
+                        className: 'text-center',
+                        render: function(data, row, type, meta) {
+                            return '<a href="/member/' + data + '/update" class="btn btn-warning btn-sm mr-2"><i class="fas fa-edit"></i></a><a href="/member/' + data + '/delete" class="btn btn-danger btn-sm" onclick="return confirm(`Are you sure you want to delete this entry?`)"><i class="fas fa-trash-alt"></i></a>';
+                        }
                     }
-                },
-                {
-                    data: 'TeamName',
-                    className: 'TeamName text-center',
-                    visible: false,
-                    searchable: true
-                },
-                {
-                    data: 'EmailAddress',
-                    className: 'EmailAddress text-center',
-                    render: function(data, type, row, meta){
-                        return '<a href="mailto:' + data + '">' + data + '</a>';
-                    }
-                },
-                {
-                    data: 'PhoneNumber',
-                    className: 'PhoneNumber text-center'
-                },
-                {
-                    data: 'Amount',
-                    className: 'Amount text-center',
-                    render: function(data, row, type, meta) {
-                        return parseInt(data);
-                    }
-                },
-                {
-                    data: 'id',
-                    className: 'text-center',
-                    render: function(data, row, type, meta) {
-                        return '<a href="/member/' + data + '/update" class="btn btn-warning btn-sm mr-2"><i class="fas fa-edit"></i></a><a href="/member/' + data + '/delete" class="btn btn-danger btn-sm" onclick="return confirm(`Are you sure you want to delete this entry?`)"><i class="fas fa-trash-alt"></i></a>';
-                    }
-                }
-            ],
-            columnDefs: [{
-                searchable: false,
-                orderable: false,
-                targets: 0
-            }],
-        });
-
-        tournamentTable.on('order.dt search.dt', function() {
-            tournamentTable.column(0, { search: 'applied', order: 'applied' }).nodes().each(function(cell, i) {
-                cell.innerHTML = i + 1;
+                ],
+                columnDefs: [{
+                    searchable: false,
+                    orderable: false,
+                    targets: 0
+                }],
             });
-        }).draw();
 
-        var column = tournamentTable.column(5);
-        $(column.footer()).html(
-            column.data().reduce(function(a, b) {
-                return parseInt(a) + parseInt(b);
-            })
-        );
-        $('#TotalText').removeClass('text-center');
-        $('#TotalAmount').removeClass('text-center');
+            tournamentTable.on('order.dt search.dt', function() {
+                tournamentTable.column(0, { search: 'applied', order: 'applied' }).nodes().each(function(cell, i) {
+                    cell.innerHTML = i + 1;
+                });
+            }).draw();
+
+            var column = tournamentTable.column(5);
+            $(column.footer()).html(
+                column.data().reduce(function(a, b) {
+                    return parseInt(a) + parseInt(b);
+                })
+            );
+            $('#TotalText').removeClass('text-center');
+            $('#TotalAmount').removeClass('text-center');
+        }
     }
-}
 
     $.ajax({
         url: '/api/members',
@@ -93,7 +94,7 @@ $(document).ready(function() {
             callback();
         },
         success: function(res) {
-            if ($('#tournamentTable').length > 0){
+            if ($('#tournamentTable').length > 0) {
                 renderMemberTable(res);
             }
 
@@ -138,4 +139,31 @@ $(document).ready(function() {
         }
     });
 
+    $('#teamMember').validate({
+        rules: {
+            Name: {
+                required: true,
+                normalizer: function(value) { return $.trim(value); }
+            },
+            TeamName: {
+                required: true,
+                normalizer: function(value) { return $.trim(value); }
+            },
+            EmailAddress: {
+                required: true,
+                normalizer: function(value) { return $.trim(value); }
+            },
+            PhoneNumber: {
+                required: true,
+                normalizer: function(value) { return $.trim(value); }
+            },
+        },
+        messages: {
+            Name: "We need your name",
+            TeamName: "To which Team do you belong again ?",
+            EmailAddress: "Because we might need to reach you via email",
+            PhoneNumber: "Buddha prefers phone calls, sorry"
+        },
+        ignore: ':hidden:not([class~=selectized]),:hidden > .selectized, .selectize-control .selectize-input input'
+    });
 });
